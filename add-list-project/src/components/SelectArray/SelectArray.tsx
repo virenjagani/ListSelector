937b9c3b7c916ca2bbaf56f11181f10e787289ec
virenjagani <virenjagani0303@gmail.com>
2024-02-24 02:56:27 -0500

    inputInArray condition

--- add-list-project/src/components/SelectArray/SelectArray.tsx
+++ add-list-project/src/components/SelectArray/SelectArray.tsx
@@ -2,27 +2,28 @@ import type { Props } from "./SelectArray.d";
 
 const SelectArray: React.FC<Props> = ({ inputInArray, setInputInArray }) => {
   const handleSelectButton = (item: number) => {
-
-    const newArray = inputInArray.filter(i=> i !== item)
+    const newArray = inputInArray.filter((i) => i !== item);
     setInputInArray(newArray);
   };
 
   return (
-    <div
-      style={{
-        width: "300px",
-        background: "grey",
-        border: "2px solid black",
-      }}
-    >
-      {inputInArray.length > 0
-        ? inputInArray.map((item, index) => (
+    <>
+      {inputInArray.length > 0 && (
+        <div
+          style={{
+            width: "300px",
+            background: "grey",
+            border: "2px solid black",
+          }}
+        >
+          {inputInArray.map((item, index) => (
             <button onClick={() => handleSelectButton(item)} key={index}>
               Item {item}
             </button>
-          ))
-        : null}
-    </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
